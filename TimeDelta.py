8d74c76768b1402d2fe8484a5187a39cc40d3265
mikem2314 <mikem2314@gmail.com>
2019-01-03 17:50:43 -0500

    initial commit

--- TimeDelta.py
+++ TimeDelta.py
@@ -1,10 +1,18 @@
 #Written to solve https://www.hackerrank.com/challenges/python-time-delta/problem
 
 
+import datetime
 
-import arrow
+formatString = "%a %d %b %Y %H:%M:%S %z"
+testCases = int(input())
 
-time = "Sun 10 May 2015 13:54:36 -0700"
-testTime = arrow.get(time, 'ddd D MMMM YYYY HH:mm:ss Z')
+for t in range(testCases):
+    t1 = str(input())
+    t2 = str(input())
+    formatT1 = datetime.datetime.strptime(t1, formatString)
+    formatT2 = datetime.datetime.strptime(t2, formatString)
+    print (formatT1)
+    print (formatT2)
+    diff = formatT2 - formatT1
 
-print(testTime)
+    print (int(abs(diff.total_seconds())))
